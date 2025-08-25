39e763ac0a86cefe4cd7260860084d87f4f14a84
UdoNieuw <udonieuwmeijer@gmail.com>
2025-08-25 18:32:38 +0200

    Player functions

--- src/Player.java
+++ src/Player.java
@@ -8,4 +8,28 @@ public class Player {
         this.health = 20;
         this.score = 0;
     }
+
+    public String getName() {
+        return playerName;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void takeDamage() {
+        health = health - damage;  
+    }
+
+    public void addScore(){
+        score = score + points;
+    }
+
+    public boolean isAlive(){
+        return health > 0;
+    }
 }
