ef489e6d4d3e6d08723a1fea98947eff2c254e0a
UdoNieuw <udonieuwmeijer@gmail.com>
2025-08-25 14:10:58 +0200

    first commit

---
+++ src/Game.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+//MAIN game class
+public class Game {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //Get player's name
+        System.out.println("Enter your legend's name: ");
+        String playerName = scanner.nextLine();
+    }
+}
